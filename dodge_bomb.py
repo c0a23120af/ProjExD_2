bcc795cc2c7b0b1decf9a4ebbcf05388d30a179a
C0A23120 <C0A23120af@edu.teu.ac.jp>
2024-06-18 15:12:45 +0900

    練習課題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,15 +4,16 @@ import pygame as pg
 import random
 
 WIDTH, HEIGHT = 1600, 900
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
- #移動量辞書の定義
+#移動量辞書の定義
 DELTA={
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0)
 }
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
 
 def check_bound(rct: pg.Rect)-> tuple[bool,bool]:
     """
@@ -37,15 +38,14 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     clock = pg.time.Clock()
-#爆弾の描写
-    bomb= pg.Surface((20,20))
-    pg.draw.circle(bomb, (255,0,0),(10,10),10)
+    bomb= pg.Surface((20,20))# 1辺が20の空のSurfaceを作る
+    pg.draw.circle(bomb, (255,0,0),(10,10),10)# 空のSurfaceに赤い円を描く
     bomb.set_colorkey((0, 0, 0))
     x_bomb = random.randint(0,WIDTH)
     y_bomb = random.randint(0,HEIGHT)
-    bomb_rct= bomb.get_rect()
+    bomb_rct= bomb.get_rect()# 爆弾Rect
     bomb_rct.center= x_bomb, y_bomb
-    vx , vy= +5,+5
+    vx , vy= +5,+5 #爆弾の横方向速度，縦方向速度
 
     tmr = 0
 
@@ -55,18 +55,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+          
+        if kk_rct.colliderect(bomb_rct):#衝突判定
+            return  #ゲームオーバー  
         screen.blit(bg_img, [0, 0]) 
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        screen.blit(bomb, bomb_rct)
-        bomb_rct.move_ip(vx,vy)
-        
-
         for k, v in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
-       
         """
         if key_lst[pg.K_UP]:
             sum_mv[1] -= 5
@@ -80,13 +79,14 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct)!=(True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
+        screen.blit(kk_img, kk_rct)    
         bomb_rct.move_ip(vx,vy)
         yoko,tate=check_bound(bomb_rct)
-        if not tate:
-            vx += -1
-        if not yoko:
-            vy += -1
-        screen.blit(kk_img, kk_rct)
+        if not tate:#縦方向にはみ出たら
+            vy *= -1
+        if not yoko:#横方向にはみ出たら
+            vx *= -1 
+        screen.blit(bomb, bomb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
